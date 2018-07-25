7f5b9369019657fde65ff69c329c0d3329731534
Hannah <hrglasser94@gmail.com>
2018-07-25 13:47:08 -0400

    Added closure to main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 //Global variables
 var container = document.querySelector('.image-list');
 var lightbox = document.querySelector('.lightbox');
-var x = document.querySelector('.close');
 var left = document.querySelector('.left-arrow');
 var right = document.querySelector('.right-arrow');
-var index;
 var menu = document.querySelector('#menu');
 var menuPopup = document.querySelector('.menu-popup');
 
@@ -18,43 +16,54 @@ var images = [
     {url: 'images/gracie7.jpg'}, 
     {url: 'images/gracievid.gif'}
 ]
+var currentIndex;
+
+//Image List creation
+for (var i = 0; i < images.length; i++) {
+    (function(){
+        var image = images[i];
+        var index = i;
+        var newImage = document.createElement('img');
+        newImage.classList.add('imageSize');
+        newImage.setAttribute('src', image.url);
+    
+        var item = document.createElement('div');
+        item.appendChild(newImage);
+        item.classList.add('image');
+        container.appendChild(item);
+
+        //Modal Functions
+        var displayModal = function(event) {
+            var source = event.target.getAttribute('src');
+            currentIndex = index;
+            lightbox.classList.add('open');
+            setSource(source);
+        }
+        newImage.addEventListener('click', displayModal);
+    })();
+}
 
 //Functions
 var setSource = function(source) {
     var lightboxImg = document.querySelector('.lightbox-image');
     lightboxImg.setAttribute('src', source);
 }
-//Modal Functions
-var displayModal = function(event) {
-    var source = event.target.getAttribute('src');
-    index = event.target.getAttribute('data-index');
-    lightbox.classList.add('open');
-    setSource(source);
-}
-
-var closeModal = function(event) {
-    if (event.target !== document.querySelector('.lightbox-image') && event.target !== left && event.target !== right) {
-        lightbox.classList.remove('open');
-    }
-}
-
 //Photo Changing Function
 var changePhotos = function(event) {
     if (event.target === left) {
-        index--;
-        if (index < 0) {
-            index = (images.length - 1);
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = (images.length - 1);
         }
     } else if (event.target === right) {
-        index++;
-        if (index >= images.length) {
-            index = 0;
+        currentIndex++;
+        if (currentIndex >= images.length) {
+            currentIndex = 0;
         }
     }
-    var source = images[index].url;
+    var source = images[currentIndex].url;
     setSource(source);
 }
-
 //Menu Functions
 var displayMenu = function() {
     menuPopup.classList.add('open');
@@ -67,26 +76,17 @@ var closeMenu = function(event) {
         menu.classList.remove('close');  
     }
 }
-
-//Image List creation
-for (var i = 0; i < images.length; i++) {
-    var image = images[i];
-    var newImage = document.createElement('img');
-    newImage.classList.add('imageSize');
-    newImage.setAttribute('src', image.url);
-    newImage.setAttribute('data-index', i);
-    newImage.addEventListener('click', displayModal);
-
-    var item = document.createElement('div');
-    item.appendChild(newImage);
-    item.classList.add('image');
-    container.appendChild(item);
+var closeModal = function(event) {
+    if (event.target !== document.querySelector('#lightbox-image') && event.target !== left && event.target !== right) {
+        lightbox.classList.remove('open');
+    }
 }
-
-lightbox.addEventListener('click', closeModal);
-left.addEventListener('click', changePhotos);
-right.addEventListener('click', changePhotos);
 menu.addEventListener('click', displayMenu);
 menuPopup.addEventListener('click', closeMenu);
 window.addEventListener('click', closeMenu);
 window.addEventListener('scroll', closeMenu);
+left.addEventListener('click', changePhotos);
+right.addEventListener('click', changePhotos);
+lightbox.addEventListener('click', closeModal);
+
+
