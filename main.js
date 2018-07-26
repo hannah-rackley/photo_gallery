0923fbf188c7c19102a6fbf6972f47cc1596bfbf
Hannah <hrglasser94@gmail.com>
2018-07-26 11:32:16 -0400

    Changed for loop to a forEach and adjusted the affected functions.

--- main.js
+++ main.js
@@ -7,62 +7,44 @@ var menu = document.querySelector('#menu');
 var menuPopup = document.querySelector('.menu-popup');
 
 var images = [
-    {url: 'images/gracie1.jpg'},
-    {url: 'images/gracie2.jpg'},
-    {url: 'images/gracie3.jpg'},
-    {url: 'images/gracie4.jpg'}, 
-    {url: 'images/gracie5.jpg'}, 
-    {url: 'images/gracie6.jpg'}, 
-    {url: 'images/gracie7.jpg'}, 
-    {url: 'images/gracievid.gif'}
+    {url: 'images/gracie1.jpg', caption: 'Gracie close-up plus a bunch of extra text to determine if my caption will wrap the text within the image.'},
+    {url: 'images/gracie2.jpg', caption: 'Gracie distant stare'},
+    {url: 'images/gracie3.jpg', caption: 'Gracie after a bath'},
+    {url: 'images/gracie4.jpg', caption: 'Gracie enjoying the sun'}, 
+    {url: 'images/gracie5.jpg', caption: 'Gracie with fancy paws'}, 
+    {url: 'images/gracie6.jpg', caption: 'Gracie covered in mud'}, 
+    {url: 'images/gracie7.jpg', caption: 'Gracie super sassy'}, 
+    {url: 'images/gracievid.gif', caption: 'Gracie living life'}
 ]
 var currentIndex;
 
 //Image List creation
-for (var i = 0; i < images.length; i++) {
-    (function(){
-        var image = images[i];
-        var index = i;
-        var newImage = document.createElement('img');
-        newImage.classList.add('imageSize');
-        newImage.setAttribute('src', image.url);
-    
-        var item = document.createElement('div');
-        item.appendChild(newImage);
-        item.classList.add('image');
-        container.appendChild(item);
+images.forEach(function(image, i){
+    var newImage = document.createElement('img');
+    var caption = document.createElement('p');
+    newImage.classList.add('imageSize');
+    newImage.setAttribute('src', image.url);
+    caption.textContent = image.caption;
+    caption.classList.add('caption');
+    var item = document.createElement('div');
+    item.appendChild(newImage);
+    item.appendChild(caption);
+    item.classList.add('item');
+    container.appendChild(item);
 
-        //Modal Functions
-        var displayModal = function(event) {
-            var source = event.target.getAttribute('src');
-            currentIndex = index;
-            lightbox.classList.add('open');
-            setSource(source);
-        }
-        newImage.addEventListener('click', displayModal);
-    })();
-}
-
-//Functions
-var setSource = function(source) {
-    var lightboxImg = document.querySelector('.lightbox-image');
-    lightboxImg.setAttribute('src', source);
-}
-//Photo Changing Function
-var changePhotos = function(event) {
-    if (event.target === left) {
-        currentIndex--;
-        if (currentIndex < 0) {
-            currentIndex = (images.length - 1);
-        }
-    } else if (event.target === right) {
-        currentIndex++;
-        if (currentIndex >= images.length) {
-            currentIndex = 0;
-        }
+    //Modal display function
+    var displayModal = function() {
+        currentIndex = i;
+        lightbox.classList.add('open');
+        setSource(i);
     }
-    var source = images[currentIndex].url;
-    setSource(source);
+    item.addEventListener('click', displayModal);
+});
+
+var setSource = function(index) {
+    var lightboxImg = document.querySelector('.lightbox-image');
+    var source = images[index].url
+    lightboxImg.setAttribute('src', source);
 }
 //Menu Functions
 var displayMenu = function() {
@@ -76,10 +58,28 @@ var closeMenu = function(event) {
         menu.classList.remove('close');  
     }
 }
+
+//Modal Function
 var closeModal = function(event) {
-    if (event.target !== document.querySelector('#lightbox-image') && event.target !== left && event.target !== right) {
+    if (event.target !== document.querySelector('.lightbox-image') && event.target !== left && event.target !== right && event.target) {
         lightbox.classList.remove('open');
     }
+}
+
+//Photo Changing Function
+var changePhotos = function(event) {
+    if (event.target === left) {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = (images.length - 1);
+        }
+    } else if (event.target === right) {
+        currentIndex++;
+        if (currentIndex >= images.length) {
+            currentIndex = 0;
+        }
+    }
+    setSource(currentIndex);
 }
 menu.addEventListener('click', displayMenu);
 menuPopup.addEventListener('click', closeMenu);
